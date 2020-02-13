ec7ae02a8f3e5155a975d3c171bb5ba9f0f3a475
hai.t.le16@gmail.com <hai.t.le16@gmail.com>
2020-02-13 12:59:50 -0800

    added whiteboard trace

--- java401challenges/src/main/java/java401challenges/mergeSort/MergeSort.java
+++ java401challenges/src/main/java/java401challenges/mergeSort/MergeSort.java
@@ -3,23 +3,47 @@ package java401challenges.mergeSort;
 import java.util.Arrays;
 
 public class MergeSort {
-    public static int[] mergeSort(int[] arr) {
+    public static void mergeSort(int[] arr) {
 
         int n = arr.length;
 
         if(n > 1) {
             int mid = arr.length / 2;
-            int[] left = new int[mid];
-            int[] right = new int[arr.length - left.length];
-
-            for(int i = 0; i < mid; i++){
-                left[i] = arr[i];
-                right[i] = arr[i+mid];
-            }
-            System.out.println(Arrays.toString(left));
-            System.out.println(Arrays.toString(right));
-
+            int[] left = Arrays.copyOfRange(arr,0, mid);
+            int[] right = Arrays.copyOfRange(arr, mid, n);
+            mergeSort(left);
+            mergeSort(right);
+            merge(left,right,arr);
         }
-        return arr;
+    }
+
+    public static void merge(int[] left, int[] right, int[] arr) {
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while( i < left.length && j < right.length) {
+            if(left[i] <= right[j]) {
+                arr[k] = left[i];
+                i++;
+            }else {
+                arr[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+        if(i == left.length) {
+            while(k < arr.length) {
+                arr[k] = right[j];
+                k++;
+                j++;
+            }
+        }else {
+            while(k < arr.length) {
+                arr[k] = left[i];
+                k++;
+                i++;
+            }
+        }
     }
 }
---
+++ java401challenges/src/main/java/java401challenges/quickSort/BLOG.md
@@ -0,0 +1,12 @@
+# Quick Sort
+
+### QuickSort (sometimes called partition-exchange sort) is one of the most efficient sorting algorithms and is based on the splitting of an array into smaller ones.
+
+
+
+
+
+
+
+
+![](../../../../../assets/quickSort.jpg)  
