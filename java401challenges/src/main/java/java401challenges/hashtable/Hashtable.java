a8be062da9b6410473dbcf651e157ba6d60a00a3
hai.t.le16@gmail.com <hai.t.le16@gmail.com>
2020-02-19 10:43:45 -0800

    added contains method

--- java401challenges/src/main/java/java401challenges/hashtable/Hashtable.java
+++ java401challenges/src/main/java/java401challenges/hashtable/Hashtable.java
@@ -23,13 +23,17 @@ public class Hashtable {
         myhash.add("Michael","Student");
         myhash.add("Ovi","Student");
 
+        System.out.println(myhash.contains("Benn"));
         System.out.println(myhash.toString());
+        System.out.println(myhash.get("Rosie"));
 
 
     }
 
+
+
     int size;
-    LinkedList[] map;
+    LinkedList<String>[] map;
 
     public Hashtable(int size) {
         this.size = size;
@@ -44,14 +48,13 @@ public class Hashtable {
 ////        hashValue = (hashValue * 599) % this.size;
 ////
 ////        return hashValue;
-        int hashCode = key.hashCode();
-        int hashValue = Math.abs(hashCode % this.size);
-        return hashValue;
+        return Math.abs(key.hashCode() % this.size);
 
     }
 
     public void add(String key, String value) {
         int hashKey = hash(key);
+        LinkedList newNode = new LinkedList<>();
         // if its empty hashtable
         if(this.map[hashKey]  == null) {
             this.map[hashKey] = new LinkedList<>();
@@ -59,6 +62,34 @@ public class Hashtable {
             this.map[hashKey].add(value);
         }
     }
+
+    public String get(String key) {
+        int hashKey = hash(key);
+        if(this.map[hashKey] == null) return null;
+        LinkedList<String> list = this.map[hashKey];
+
+        for (String storage : list) {
+            if (key.equals(storage)) {
+                System.out.println("it gets in here");
+                return storage;
+            }
+        }
+        System.out.println(list.toString());
+        return "No value with that key found.";
+    }
+
+    public Boolean contains(String key) {
+        int hashKey = hash(key);
+        if(this.map[hashKey] == null) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+
+
+
 
     @Override
     public String toString() {
