e77c7098118067372465181b28c5473d8a4118a5
hai.t.le16@gmail.com <hai.t.le16@gmail.com>
2020-02-19 16:58:22 -0800

    added tests

--- java401challenges/src/test/java/java401challenges/hashtable/HashtableTest.java
+++ java401challenges/src/test/java/java401challenges/hashtable/HashtableTest.java
@@ -1,7 +1,17 @@
 package java401challenges.hashtable;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class HashtableTest {
+
+    Hashtable hashtable = new Hashtable(5);
+
+    @Test
+    public void containsTest() {
+        hashtable.add("String", "Value");
+        assertTrue(hashtable.contains("String"));
+    }
 
 }
